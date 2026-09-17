58d3d0559990b29f0e1d370b1bc0b99e50ad6fb0
Nils Jivegård <nils.balassa@gmail.com>
2026-09-17 10:01:51 +0200

    Translate monster generation terms to Swedish
    Updated monster generation script to use Swedish terms for monster types, animals, features, traits, abilities, tactics, personalities, and weaknesses. Adjusted HTML output to reflect the changes.

--- assets/mazeMonster.js
+++ assets/mazeMonster.js
@@ -1,62 +1,198 @@
+```javascript
 function generateMonster() {
     const outputDiv = document.getElementById('monster-output');
     
-    // Monster Base
+    // Monsterbas
     const monsterBaseRoll = Math.floor(Math.random() * 6) + 1;
     let monsterBase;
     if (monsterBaseRoll <= 2) {
-        monsterBase = "Aerial";
+        monsterBase = "Flygande";
     } else if (monsterBaseRoll <= 4) {
-        monsterBase = "Terrestrial";
+        monsterBase = "Landlevande";
     } else {
-        monsterBase = "Aquatic";
+        monsterBase = "Vattenlevande";
     }
 
-    // Animals
-    const aerialAnimals = ["Albatross", "Bat", "Beetle", "Bird of Paradise", "Butterfly", "Condor", "Crane", "Crow", "Dragonfly", "Eagle", "Falcon", "Firefly", "Flamingo", "Fly", "Flying Squirrel", "Goose", "Gull", "Hummingbird", "Kingfisher", "Locust", "Magpie", "Mantis", "Mockingbird", "Mosquito", "Moth", "Owl", "Parrot", "Peacock", "Pelican", "Pteranodon", "Rooster", "Sparrow", "Swan", "Vulture", "Wasp", "Woodpecker"];
-    const terrestrialAnimals = ["Ant", "Ape", "Armadillo", "Badger", "Bear", "Boar", "Caterpillar", "Centipede", "Chameleon", "Cockroach", "Deer", "Elephant", "Ferret", "Fox", "Giraffe", "Goat", "Horse", "Human", "Mole", "Ostrich", "Ox", "Porcupine", "Rabbit", "Raccoon", "Rat", "Rhinoceros", "Scorpion", "Sheep", "Slug", "Snail", "Snake", "Spider", "Squirrel", "Tiger", "Wolf", "Wolverine"];
-    const aquaticAnimals = ["Alligator", "Amoeba", "Anglerfish", "Beaver", "Clam", "Crab", "Dolphin", "Eel", "Frog", "Hippopotamus", "Jellyfish", "Leech", "Lobster", "Manatee", "Manta Ray", "Muskrat", "Narwhal", "Newt", "Octopus", "Otter", "Penguin", "Platypus", "Pufferfish", "Salamander", "Sea Anemone", "Sea Urchin", "Seahorse", "Seal", "Shark", "Shrimp", "Squid", "Swordfish", "Tadpole", "Turtle", "Walrus", "Whale"];
+    // Djur
+    const aerialAnimals = [
+        "Albatross", "Fladdermus", "Skalbagge", "Tropisk fågel",
+        "Fjäril", "Kondor", "Trana", "Kråka", "Trollslända",
+        "Örn", "Falk", "Eldfluga", "Flamingo", "Fluga",
+        "Flygekorre", "Gås", "Fiskmås", "Kolibri", "Kungsfiskare",
+        "Gräshoppa", "Skata", "Syrsa", "Svartmes", "Mygga",
+        "Mal", "Uggla", "Papegoja", "Påfågel", "Pelikan",
+        "Pteranodon", "Tupp", "Gråmes", "Svan", "Gam",
+        "Geting", "Hackspett"
+    ];
+
+    const terrestrialAnimals = [
+        "Myra", "Apa", "Bältdjur", "Grävling", "Björn", "Vildsvin",
+        "Larv", "Tusenfoting", "Kameleont", "Kackerlacka", "Hjort",
+        "Elefant", "Iller", "Räv", "Giraff", "Get", "Häst",
+        "Människa", "Mullvad", "Struts", "Oxe", "Piggsvin",
+        "Kanin", "Tvättbjörn", "Råtta", "Noshörning", "Skorpion",
+        "Får", "Mördarsnigel", "Snigel", "Orm", "Spindel",
+        "Ekorre", "Tiger", "Varg", "Järv"
+    ];
+
+    const aquaticAnimals = [
+        "Alligator", "Amöba", "Marulk", "Bäver", "Mussla", "Krabba",
+        "Delfin", "Ål", "Groda", "Flodhäst", "Manet", "Igel",
+        "Hummer", "Manat", "Manta", "Bisamråtta", "Narval",
+        "Vattensalamander", "Bläckfisk", "Utter", "Pingvin",
+        "Näbbdjur", "Kulfisk", "Salamander", "Havsanemon",
+        "Sjöborre", "Sjöhäst", "Säl", "Haj", "Räka", "Kalmar",
+        "Svärdfisk", "Grodyngel", "Sköldpadda", "Valross", "Val"
+    ];
 
     let monsterAnimal;
-    if (monsterBase === "Aerial") {
+    if (monsterBase === "Flygande") {
         monsterAnimal = aerialAnimals[Math.floor(Math.random() * aerialAnimals.length)];
-    } else if (monsterBase === "Terrestrial") {
+    } else if (monsterBase === "Landlevande") {
         monsterAnimal = terrestrialAnimals[Math.floor(Math.random() * terrestrialAnimals.length)];
     } else {
         monsterAnimal = aquaticAnimals[Math.floor(Math.random() * aquaticAnimals.length)];
     }
 
-    // Monster Features
-    const monsterFeatures = ["Antlers", "Beak", "Carapace", "Claws", "Compound eyes", "Eye Stalks", "Fangs", "Fins", "Fur", "Gills", "Hooves", "Horns", "Legless", "Long tongue", "Many-eyed", "Many-limbed", "Mucus", "Pincers", "Plates", "Plumage", "Proboscis", "Scales", "Segments", "Shaggy hair", "Shell", "Spikes", "Spinnerets", "Spines", "Stinger", "Suction cups", "Tail", "Talons", "Tentacles", "Trunk", "Tusks", "Wings"];
+    // Monsterdrag
+    const monsterFeatures = [
+        "Hjorthorn", "Näbb", "Pansarsköld", "Klor",
+        "Facettögon", "Ögon på stjälkar", "Huggtänder", "Fenor",
+        "Päls", "Gälar", "Hovar", "Horn", "Benlös",
+        "Lång tunga", "Mångögd", "Månglemmad", "Slem",
+        "Tänger", "Pansarplåtar", "Fjäderdräkt", "Snabel",
+        "Fjäll", "Segmenterad kropp", "Raggig päls", "Skal",
+        "Taggar", "Spinnvårtor", "Ryggtaggar", "Gadd",
+        "Sugkoppar", "Svans", "Rovfågelsklo", "Tentakler",
+        "Snabel", "Betar", "Vingar"
+    ];
     const monsterFeature = monsterFeatures[Math.floor(Math.random() * monsterFeatures.length)];
 
-    // Monster Traits
-    const monsterTraits = ["Amphibious", "Bloated", "Brittle", "Cannibal", "Clay-like", "Colossal", "Crystalline", "Decaying", "Ether. Element", "Ethereal", "Ever-young", "Eyeless", "Fearless", "Fluffy", "Fungal", "Gelatinous", "Geometric", "Hardened", "Illusory", "Intelligent", "Iridescent", "Luminous", "Many-headed", "Mechanical", "Phys. Element", "Planar", "Reflective", "Rubbery", "Shadowy", "Sharp", "Skeletal", "Slimy", "Sticky", "Stinking", "Tiny", "Translucent"];
+    // Monstersärdrag
+    const monsterTraits = [
+        "Amfibisk", "Uppsvälld", "Skör", "Kannibalisk",
+        "Lerartad", "Kolossal", "Kristallartad", "Förmultnad",
+        "Eteriskt väsen", "Eterisk", "Evigt ung", "Ögonlös",
+        "Orädd", "Fluffig", "Svampbevuxen", "Geléartad",
+        "Geometrisk", "Förhärdad", "Illusorisk", "Intelligent",
+        "Iriscerande", "Självlysande", "Månghövdad", "Mekanisk",
+        "Fysiskt väsen", "Planbunden", "Spegelblank",
+        "Gummiartad", "Skuggskimrande", "Rakbladsvass",
+        "Skelettartad", "Slemmig", "Klistrig", "Stinkande",
+        "Pysslingliten", "Genomskinlig"
+    ];
     const monsterTrait = monsterTraits[Math.floor(Math.random() * monsterTraits.length)];
 
-    // Monster Abilities
-    const monsterAbilities = ["Absorbing", "Acid blood", "Anti-magic", "Blinding", "Breath weapon", "Camouflaging", "Duplicating", "Electric", "Entangling", "Ethereal Effect", "Exploding", "Flying", "Gaze weapon", "Hypnotizing", "Impervious", "Invisible", "Life-draining", "Magnetic", "Mimicking", "Mind-Reading", "Paralyzing", "Phasing", "Physical Effect", "Poisonous", "Radioactive", "Reflective", "Regenerating", "Shapeshifting", "Spell-casting", "Stealthy", "Strangling", "Super-strength", "Telekinetic", "Teleporting", "Vampiric", "Wall-Crawling"];
+    // Monsterförmågor
+    const monsterAbilities = [
+        "Absorberar", "Frätande blod", "Antimagi", "Förblindande",
+        "Andedräktsvapen", "Kamouflage", "Dubbelgångare",
+        "Elektrisk kraft", "Snärjande", "Eterisk kraft",
+        "Exploderar", "Flyger", "Förhäxande blick", "Hypnotiserar",
+        "Ogenomtränglig", "Osynlig", "Dränerar livskraft",
+        "Magnetisk", "Härmar", "Läser tankar", "Paralyserar",
+        "Fasförskjutning", "Övernaturlig kraft", "Giftig",
+        "Radioaktiv", "Reflekterande", "Regenererar",
+        "Formskiftande", "Besvärjelsekonst", "Smygande",
+        "Strypgrepp", "Övernaturlig styrka", "Telekinesi",
+        "Teleportation", "Vampyrisk", "Väggklättrare"
+    ];
     const monsterAbility = monsterAbilities[Math.floor(Math.random() * monsterAbilities.length)];
 
-    // Monster Tactics
-    const monsterTactics = ["Ambush", "Call for support", "Capture", "Charge", "Climb foes", "Compel worship", "Create barrier", "Deceive", "Demand duel", "Disorient", "Encircle", "Evade", "Gang up", "Gather strength", "Go berserk", "Harry", "Hurl foes", "Immobilize", "Manipulate", "Mock", "Monologue", "Order minion", "Protect leader", "Protect self", "Scatter foes", "Stalk", "Steal from", "Swarm", "Target insolent", "Target leader", "Target nearest", "Target richest", "Target strongest", "Target weakest", "Toy with", "Use terrain"];
+    // Monster-taktik
+    const monsterTactics = [
+        "Överfall", "Kalla på förstärkning", "Fånga", "Storma",
+        "Klättra på fiender", "Kräva dyrkan", "Skapa barriär",
+        "Bedra", "Utmana till duell", "Desorientera", "Omringa",
+        "Fly undan", "Gå till angrepp i flock", "Samla kraft",
+        "Gå bärsärk", "Trakassera", "Slunga fiender", "Immobilisera",
+        "Manipulera", "Håna", "Hålla en monolog", "Befalla en undersåte",
+        "Skydda ledaren", "Skydda sig själv", "Splittra fiender",
+        "Förfölja", "Stjäla från", "Översvämma med horder",
+        "Ge sig på den oförskämde", "Ge sig på ledaren",
+        "Ge sig på den närmaste", "Ge sig på den rikaste",
+        "Ge sig på den starkaste", "Ge sig på den svagaste",
+        "Leka med sitt byte", "Utnyttja terrängen"
+    ];
     const monsterTactic = monsterTactics[Math.floor(Math.random() * monsterTactics.length)];
 
-    // Monster Personalities
-    const monsterPersonalities = ["Alien", "Aloof", "Bored", "Cautious", "Cowardly", "Curious", "Devious", "Distractible", "Educated", "Embittered", "Envious", "Erudite", "Fanatical", "Forgetful", "Generous", "Hateful", "Honorable", "Humble", "Jaded", "Jovial", "Legalistic", "Manipulative", "Megalomaniac", "Melancholy", "Meticulous", "Mystical", "Obsessive", "Out of Touch", "Paranoid", "Polite", "Psychopathic", "Sophisticated", "Touchy", "Unimpressed", "Vain", "Xenophobic"];
+    // Monsterpersonlighet
+    const monsterPersonalities = [
+        "Främmande", "Avmätt", "Uttråkad", "Försiktig",
+        "Feg", "Nyfiken", "Listig", "Lättdistraherad",
+        "Bildad", "Förbittrad", "Avundsjuk", "Lärd",
+        "Fanatisk", "Glömsk", "Generös", "Hatiskt lagd",
+        "Ärofull", "Ödmjuk", "Förhärdad", "Munter",
+        "Lagvrängare", "Manipulativ", "Storhetsvansinnig",
+        "Melankolisk", "Pedantisk", "Mystisk", "Besatt",
+        "Verklighetsfrånvänd", "Paranoid", "Artig",
+        "Skrämmande kall", "Förfinad", "Lättstött",
+        "Oimponerad", "Fåfäng", "Främlingsfientlig"
+    ];
     const monsterPersonality = monsterPersonalities[Math.floor(Math.random() * monsterPersonalities.length)];
 
-    // Monster Weakness
-    const monsterWeaknesses = ["Bells", "Birdsong", "Children", "Cold", "Cold Iron", "Competition", "Conversation", "Deformity", "Flattery", "Flowers", "Gifts", "Gold", "Heat", "Holy Icon", "Holy Water", "Home Cooking", "Insanities", "Mirrors", "Mistletoe", "Moonlight", "Music", "Methods", "Phylactery", "Phys. Elements", "Puzzles", "Riddles", "Rituals", "Silver", "Sunlight", "Tears", "True Name", "Val. Materials", "Weak Spot", "Weapon Items", "Wine", "Wormwood"];
+    // Monstersvaghet
+    const monsterWeaknesses = [
+        "Klockor", "Fågelsång", "Barn", "Kyla", "Kalljärn",
+        "Konkurrens", "Samtal", "Missbildningar", "Smicker",
+        "Blommor", "Gåvor", "Guld", "Värme", "Heliga symboler",
+        "Heligt vatten", "Husmanskost", "Galenskap", "Speglar",
+        "Mistel", "Månljus", "Musik", "Metoder", "Fylakterium",
+        "Fysiska element", "Pussel", "Gåtor", "Ritualer",
+        "Silver", "Solljus", "Tårar", "Sanna namn",
+        "Värdefulla material", "Svag punkt", "Vapen",
+        "Vin", "Malört"
+    ];
     const monsterWeakness = monsterWeaknesses[Math.floor(Math.random() * monsterWeaknesses.length)];
 
-    // Stats (slumpar en typ av varje)
-    const health = ["Weak: 1d", "Typical: 2d", "Tough: 3d", "Hulking: 4d", "Colossal: 6d"];
-    const armor = ["Unarmored: 6 armor", "Light protection: 7 armor", "Moderate protection: 8 armor", "Heavy protection: 9 armor", "Nigh impervious: 10 armor"];
-    const attackBonus = ["Untrained: +0 AB", "Trained: +1 AB", "Dangerous: +2 AB", "Masterful: +3 AB", "Lethal: +4 AB"];
-    const strBonus = ["Weak: +0 STR", "Average: +1 STR", "Strong: +2 STR", "Powerful: +3 STR", "Monstrous: +4 STR"];
-    const dexBonus = ["Slow: +0 DEX", "Average: +1 DEX", "Nimble: +2 DEX", "Swift: +3 DEX", "Blurred: +4 DEX"];
-    const wilBonus = ["Dimwitted: +0 WIL", "Average: +1 WIL", "Clever: +2 WIL", "Brilliant: +3 WIL", "Genius: +4 WIL"];
+    // Statistik
+    const health = [
+        "Svag: 1t",
+        "Normal: 2t",
+        "Tålig: 3t",
+        "Bjässelik: 4t",
+        "Kolossal: 6t"
+    ];
+
+    const armor = [
+        "Obepansrad: 6 rustning",
+        "Lätt skydd: 7 rustning",
+        "Medeltungt skydd: 8 rustning",
+        "Tungt skydd: 9 rustning",
+        "Nästan ogenomtränglig: 10 rustning"
+    ];
+
+    const attackBonus = [
+        "Otränad: +0 AB",
+        "Tränad: +1 AB",
+        "Farlig: +2 AB",
+        "Mästerlig: +3 AB",
+        "Dödlig: +4 AB"
+    ];
+
+    const strBonus = [
+        "Svag: +0 STY",
+        "Medelmåttig: +1 STY",
+        "Stark: +2 STY",
+        "Kraftfull: +3 STY",
+        "Monstruös: +4 STY"
+    ];
+
+    const dexBonus = [
+        "Långsam: +0 SMI",
+        "Medelmåttig: +1 SMI",
+        "Smidig: +2 SMI",
+        "Snabb: +3 SMI",
+        "Oskymtbar: +4 SMI"
+    ];
+
+    const wilBonus = [
+        "Trög: +0 VIL",
+        "Medelmåttig: +1 VIL",
+        "Klipsk: +2 VIL",
+        "Briljant: +3 VIL",
+        "Genialisk: +4 VIL"
+    ];
 
     const randomHealth = health[Math.floor(Math.random() * health.length)];
     const randomArmor = armor[Math.floor(Math.random() * armor.length)];
@@ -65,10 +201,10 @@ function generateMonster() {
     const randomDexBonus = dexBonus[Math.floor(Math.random() * dexBonus.length)];
     const randomWilBonus = wilBonus[Math.floor(Math.random() * wilBonus.length)];
     
-    // Generate HTML output
+    // Generera HTML-utdata
     const outputHTML = `
         <div style="border: 1px solid #ddd; padding: 15px; border-radius: 5px; background-color: #f9f9f9;">
-            <h3>Slumpat Monster</h3>
+            <h3>Slumpat monster</h3>
             <h4>Grundläggande</h4>
             <p><strong>Typ:</strong> ${monsterBase}</p>
             <p><strong>Djur:</strong> ${monsterAnimal}</p>
@@ -77,7 +213,7 @@ function generateMonster() {
             <ul>
                 <li><strong>Hälsa:</strong> ${randomHealth}</li>
                 <li><strong>Rustning:</strong> ${randomArmor}</li>
-                <li><strong>Attack Bonus:</strong> ${randomAttackBonus}</li>
+                <li><strong>Attackbonus:</strong> ${randomAttackBonus}</li>
                 <li><strong>Styrka:</strong> ${randomStrBonus}</li>
                 <li><strong>Smidighet:</strong> ${randomDexBonus}</li>
                 <li><strong>Vilja:</strong> ${randomWilBonus}</li>
@@ -85,15 +221,16 @@ function generateMonster() {
 
             <h4>Detaljer</h4>
             <ul>
-                <li><strong>Egenskap (Feature):</strong> ${monsterFeature}</li>
-                <li><strong>Särdrag (Trait):</strong> ${monsterTrait}</li>
-                <li><strong>Förmåga (Ability):</strong> ${monsterAbility}</li>
+                <li><strong>Egenskap:</strong> ${monsterFeature}</li>
+                <li><strong>Särdrag:</strong> ${monsterTrait}</li>
+                <li><strong>Förmåga:</strong> ${monsterAbility}</li>
                 <li><strong>Taktik:</strong> ${monsterTactic}</li>
                 <li><strong>Personlighet:</strong> ${monsterPersonality}</li>
-                <li><strong>Svaghet (Weakness):</strong> ${monsterWeakness}</li>
+                <li><strong>Svaghet:</strong> ${monsterWeakness}</li>
             </ul>
         </div>
     `;
     
     outputDiv.innerHTML = outputHTML;
 }
+```
